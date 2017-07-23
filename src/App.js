9a232c60ed118136d53bc4b006fb8b0897aa563d
Thomas <thomas.ford@outlook.com>
2017-07-23 17:04:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <table className="table">
+              <thead></thead>
+            </table>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
